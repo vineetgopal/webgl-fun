261e501d4e290fa3efdd2c4c9da148b4e419817f
Vineet Gopal <vineetgopal92@gmail.com>
2014-06-08 16:23:02 -0500

    texture

--- testing.js
+++ testing.js
@@ -2,12 +2,13 @@ var canvas;
 var gl;
 var squareVerticesBuffer;
 var squareVerticesColorBuffer;
+var cubeTexture;
 var mvMatrix;
 var shaderProgram;
 var vertexPositionAttribute;
 var vertexColorAttribute;
 var perspectiveMatrix;
-var squareRotation = 0.0;
+var cubeRotation = 0.0;
 var lastSquareUpdateTime;
 var squareXOffset = 0.0;
 var squareYOffset = 0.0;
@@ -26,6 +27,7 @@ function start() {
 
   initShaders();
   initBuffers();
+  initTextures()
 
   setInterval(drawScene, 15);
 }
@@ -46,8 +48,8 @@ function initShaders() {
   vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
   gl.enableVertexAttribArray(vertexPositionAttribute);
 
-  vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-  gl.enableVertexAttribArray(vertexColorAttribute);
+  textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+  gl.enableVertexAttribArray(textureCoordAttribute);
 }
 
 function getShader(gl, id) {
@@ -135,28 +137,44 @@ function initBuffers() {
   
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  var colors = [
-    [1.0,  1.0,  1.0,  1.0],    // Front face: white
-    [1.0,  0.0,  0.0,  1.0],    // Back face: red
-    [0.0,  1.0,  0.0,  1.0],    // Top face: green
-    [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-    [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-    [1.0,  0.0,  1.0,  1.0]     // Left face: purple
+  cubeVerticesTextureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesTextureCoordBuffer);
+
+  var textureCoordinates = [
+    // Front
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Back
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Top
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Bottom
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Right
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0,
+    // Left
+    0.0,  0.0,
+    1.0,  0.0,
+    1.0,  1.0,
+    0.0,  1.0
   ];
 
-  var generatedColors = [];
-  
-  for (j=0; j<6; j++) {
-    var c = colors[j];
-    
-    for (var i=0; i<4; i++) {
-      generatedColors = generatedColors.concat(c);
-    }
-  }
-
-  cubeVerticesColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generatedColors), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+                gl.STATIC_DRAW);
 
   cubeVerticesIndexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
@@ -188,34 +206,38 @@ function drawScene() {
   loadIdentity();
   mvTranslate([-0.0, 0.0, -6.0]);
   mvPushMatrix();
-  mvRotate(squareRotation, [1, 0, 1]);
-  mvTranslate([squareXOffset, squareYOffset, squareZOffset]);
+  mvRotate(cubeRotation, [1, 0, 1]);
+
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
   gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
-  gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesTextureCoordBuffer);
+  gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, cubeTexture);
+  gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
   setMatrixUniforms();
   gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
-  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
   mvPopMatrix();
 
   var currentTime = (new Date).getTime();
   if (lastSquareUpdateTime) {
     var delta = currentTime - lastSquareUpdateTime;
     
-    squareRotation += (30 * delta) / 1000.0;
-    squareXOffset += xIncValue * ((30 * delta) / 1000.0);
-    squareYOffset += yIncValue * ((30 * delta) / 1000.0);
-    squareZOffset += zIncValue * ((30 * delta) / 1000.0);
+    cubeRotation += (30 * delta) / 1000.0;
+    // squareXOffset += xIncValue * ((30 * delta) / 1000.0);
+    // squareYOffset += yIncValue * ((30 * delta) / 1000.0);
+    // squareZOffset += zIncValue * ((30 * delta) / 1000.0);
     
-    if (Math.abs(squareYOffset) > 2.5) {
-      xIncValue = -xIncValue;
-      yIncValue = -yIncValue;
-      zIncValue = -zIncValue;
-    }
+    // if (Math.abs(squareYOffset) > 2.5) {
+    //   xIncValue = -xIncValue;
+    //   yIncValue = -yIncValue;
+    //   zIncValue = -zIncValue;
+    // }
   }
   
   lastSquareUpdateTime = currentTime;
@@ -267,3 +289,20 @@ function mvRotate(angle, v) {
   var m = Matrix.Rotation(inRadians, $V([v[0], v[1], v[2]])).ensure4x4();
   multMatrix(m);
 } 
+
+function initTextures() {
+  cubeTexture = gl.createTexture();
+  cubeImage = new Image();
+  cubeImage.crossOrigin="anonymous";
+  cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); };
+  cubeImage.src = "https://lh3.googleusercontent.com/-MNIlvSWayiU/AAAAAAAAAAI/AAAAAAAAAAA/NCelWc3s7yw/photo.jpg";
+}
+
+function handleTextureLoaded(image, texture) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
